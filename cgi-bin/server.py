6fef738a0f775fc7eba26579cb3fe039a28b28c5
baokui <sy880115>
2020-03-05 22:20:12 +0800

    generate

--- cgi-bin/server.py
+++ cgi-bin/server.py
@@ -1,14 +1,13 @@
 from wsgiref.simple_server import make_server
 import sys
 # 导入我们自己编写的application函数:
-port = sys.argv[1]
 from test import application
 
 # 创建一个服务器，IP地址为空，端口是8000，处理函数是application:
-
-httpd = make_server('', port, application)
-
-print("Serving HTTP on port %s..."%port)
+if __name__=="__main__":
+    port = sys.argv[1]
+    httpd = make_server('', port, application)
+    print("Serving HTTP on port %s..."%port)
 
 # 开始监听HTTP请求:
 
