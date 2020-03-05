2ea80447ea9d47ccb2df46d30a8cad97ee73cbf8
baokui <sy880115>
2020-03-05 21:40:57 +0800

    generate

--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -199,7 +199,9 @@ def application(environ, start_response):
     # in the file like wsgi.input environment variable.
     request_body = environ['wsgi.input'].read(request_body_size)
     request_body = str(request_body, encoding="utf-8")
+    print(request_body)
     request_body = unquote(request_body, encoding="GBK")
+    print(request_body)
     d = urllib.parse.parse_qs(request_body,encoding='utf-8')
     if len(d)>=1:
         inputStr = d.get('inputStr', [''])[0]  # Returns the first age value.
