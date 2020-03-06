16712f6d80b2c2506111aac6a2aa257f3528ba91
baokui <sy880115>
2020-03-06 23:00:06 +0800

    get data

--- getInputData/run_datapro.sh
+++ getInputData/run_datapro.sh
@@ -1,1 +1,5 @@
-nohup python -u datapro.py ../data/sogouInput/0000 0000 >> log/0000.log 2>&1 &
+for((i=1;i<10;i++))
+do
+name=000$i
+nohup python -u datapro.py ../data/sogouInput/$name $name >> log/$name.log 2>&1 &
+done
