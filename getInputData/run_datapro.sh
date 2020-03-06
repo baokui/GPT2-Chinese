629ebcc096ad8463e6566ed796556c7adca26fbc
baokui <sy880115>
2020-03-06 23:27:29 +0800

    get data

--- getInputData/run_datapro.sh
+++ getInputData/run_datapro.sh
@@ -1,5 +1,27 @@
-for((i=1;i<10;i++))
+sleep 3h
+for((i=1;i<9;i++))
 do
-name=000$i
+name=001$i
 nohup python -u datapro.py ../data/sogouInput/$name $name >> log/$name.log 2>&1 &
 done
+i=9
+name=001$i
+nohup python -u datapro.py ../data/sogouInput/$name $name >> log/$name.log 2>&1
+
+for((i=1;i<9;i++))
+do
+name=002$i
+nohup python -u datapro.py ../data/sogouInput/$name $name >> log/$name.log 2>&1 &
+done
+i=9
+name=002$i
+nohup python -u datapro.py ../data/sogouInput/$name $name >> log/$name.log 2>&1
+
+for((i=1;i<9;i++))
+do
+name=003$i
+nohup python -u datapro.py ../data/sogouInput/$name $name >> log/$name.log 2>&1 &
+done
+i=9
+name=003$i
+nohup python -u datapro.py ../data/sogouInput/$name $name >> log/$name.log 2>&1
