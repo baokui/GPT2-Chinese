18f4fb5887431f49e094f7d5cb7168b738da02fb
baokui <sy880115>
2020-03-06 18:39:44 +0800

    generate

--- getInputData/run_main.sh
+++ getInputData/run_main.sh
@@ -7,13 +7,13 @@ do
         idx=$k$i
         input=zhangfeixue/shurufa/st_data/$Month/00$idx*
         output=VpaOutput_guobk/dabaigou_train/$Month/00$idx
-        #nohup sh run_join.sh $input $output >> log/$Month-00$idx-.log 2>&1 &
+        nohup sh run_join.sh $input $output >> log/$Month-00$idx-.log 2>&1 &
         echo $input
     done
     i=9
     idx=$k$i
     input=zhangfeixue/shurufa/st_data/$Month/00$idx*
     output=VpaOutput_guobk/dabaigou_train/$Month/00$idx
-    #nohup sh run_join.sh $input $output >> log/$Month-00$idx-.log 2>&1
+    nohup sh run_join.sh $input $output >> log/$Month-00$idx-.log 2>&1
     echo $input
 done
