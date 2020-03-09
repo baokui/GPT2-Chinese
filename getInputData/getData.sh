788ef2cc91a0ad981fc705e2c9aa29ddc634bed7
baokui <sy880115>
2020-03-09 22:13:36 +0800

    get data

--- getInputData/getData.sh
+++ getInputData/getData.sh
@@ -1,5 +1,5 @@
 hadoopclients=ml_research,3evrqV2R
 for((i=0;i<10;i++))
 do
-hadoop fs -Dhadoop.client.ugi=$hadoopclients -get VpaOutput_guobk/dabaigou_train/202001/000$i data/202001/
+hadoop fs -Dhadoop.client.ugi=$hadoopclients -get VpaOutput_guobk/dabaigou_train/202001/000$i data/dabaigou/202001/
 done
