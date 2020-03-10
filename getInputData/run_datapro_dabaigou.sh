9b3bebc552b315b27e9c686e1bc842352e67569f
baokui <sy880115>
2020-03-10 21:56:36 +0800

    get data

---
+++ getInputData/run_datapro_dabaigou.sh
@@ -0,0 +1,37 @@
+
+filename=202001
+for((i=0;i<9;i++))
+do
+name=000$i
+nohup python -u datapro_dabaigou.py ../data/$filename/$name $name >> log/$name.log 2>&1 &
+done
+i=9
+name=000$i
+nohup python -u datapro_dabaigou.py ../data/$filename/$name $name >> log/$name.log 2>&1
+
+for((i=0;i<9;i++))
+do
+name=001$i
+nohup python -u datapro_dabaigou.py ../data/$filename/$name $name >> log/$name.log 2>&1 &
+done
+i=9
+name=001$i
+nohup python -u datapro_dabaigou.py ../data/$filename/$name $name >> log/$name.log 2>&1
+
+for((i=0;i<9;i++))
+do
+name=002$i
+nohup python -u datapro_dabaigou.py ../data/$filename/$name $name >> log/$name.log 2>&1 &
+done
+i=9
+name=002$i
+nohup python -u datapro_dabaigou.py ../data/$filename/$name $name >> log/$name.log 2>&1
+
+for((i=1;i<9;i++))
+do
+name=003$i
+nohup python -u datapro_dabaigou.py ../data/$filename/$name $name >> log/$name.log 2>&1 &
+done
+i=9
+name=003$i
+nohup python -u datapro_dabaigou.py ../data/$filename/$name $name >> log/$name.log 2>&1
