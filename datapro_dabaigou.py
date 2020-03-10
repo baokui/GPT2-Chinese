be11a3faf013a8bdeb690a977c1660b443cf70c8
baokui <sy880115>
2020-03-10 21:54:37 +0800

    get data

---
+++ datapro_dabaigou.py
@@ -0,0 +1,108 @@
+#-- coding:UTF-8 --
+import transformers
+import torch
+import os
+import json
+import random
+import numpy as np
+import argparse
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+from tqdm import tqdm
+from torch.nn import DataParallel
+from tokenizations.bpe_tokenizer import get_encoder
+
+
+def build_files(data_path, tokenized_data_path, num_pieces, full_tokenizer, min_length, seq_len):
+    with open(data_path, 'r', encoding='utf8') as f:
+        print('reading lines')
+        lines = json.load(f)
+        lines = [line.replace('\n', ' [SEP] ') for line in lines]  # 用[SEP]表示换行, 段落之间使用SEP表示段落结束
+    all_len = len(lines)
+    if not os.path.exists(tokenized_data_path):
+        os.mkdir(tokenized_data_path)
+    for i in tqdm(range(num_pieces)):
+        sublines = lines[all_len // num_pieces * i: all_len // num_pieces * (i + 1)]
+        if i == num_pieces - 1:
+            sublines.extend(lines[all_len // num_pieces * (i + 1):])  # 把尾部例子添加到最后一个piece
+        sublines = [full_tokenizer.tokenize(line) for line in sublines if
+                    len(line) > min_length]  # 只考虑长度超过min_length的句子
+        sublines = [full_tokenizer.convert_tokens_to_ids(line) for line in sublines]
+        full_line = []
+        for subline in sublines:
+            tmp = [full_tokenizer.convert_tokens_to_ids('[MASK]')]
+            tmp += subline
+            if len(tmp)<seq_len-1:
+                tmp += [full_tokenizer.convert_tokens_to_ids('[PAD]')]*(seq_len-1-len(tmp))
+            else:
+                tmp = tmp[:seq_len-1]
+            full_line = tmp + [full_tokenizer.convert_tokens_to_ids('[CLS]')]  # 文章之间添加CLS表示文章结束
+        with open(tokenized_data_path + 'tokenized_train_{}.txt'.format(i), 'w') as f:
+            for id in full_line:
+                f.write(str(id) + ' ')
+    print('finish')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', default='0,1,2,3', type=str, required=False, help='设置使用哪些显卡')
+    parser.add_argument('--model_config', default='config/model_config_small.json', type=str, required=False,
+                        help='选择模型参数')
+    parser.add_argument('--tokenizer_path', default='cache/vocab_small.txt', type=str, required=False, help='选择词库')
+    parser.add_argument('--raw_data_path', default='data/train.json', type=str, required=False, help='原始训练语料')
+    parser.add_argument('--tokenized_data_path', default='data/tokenized/', type=str, required=False,
+                        help='tokenized语料存放位置')
+    parser.add_argument('--raw', action='store_true', help='是否先做tokenize')
+    parser.add_argument('--epochs', default=5, type=int, required=False, help='训练循环')
+    parser.add_argument('--batch_size', default=8, type=int, required=False, help='训练batch size')
+    parser.add_argument('--lr', default=1.5e-4, type=float, required=False, help='学习率')
+    parser.add_argument('--warmup_steps', default=2000, type=int, required=False, help='warm up步数')
+    parser.add_argument('--log_step', default=1, type=int, required=False, help='多少步汇报一次loss，设置为gradient accumulation的整数倍')
+    parser.add_argument('--stride', default=768, type=int, required=False, help='训练时取训练数据的窗口步长')
+    parser.add_argument('--gradient_accumulation', default=1, type=int, required=False, help='梯度积累')
+    parser.add_argument('--fp16', action='store_true', help='混合精度')
+    parser.add_argument('--fp16_opt_level', default='O1', type=str, required=False)
+    parser.add_argument('--max_grad_norm', default=1.0, type=float, required=False)
+    parser.add_argument('--num_pieces', default=100, type=int, required=False, help='将训练语料分成多少份')
+    parser.add_argument('--min_length', default=20, type=int, required=False, help='最短收录文章长度')
+    parser.add_argument('--output_dir', default='model/', type=str, required=False, help='模型输出路径')
+    parser.add_argument('--pretrained_model', default='', type=str, required=False, help='模型训练起点路径')
+    parser.add_argument('--writer_dir', default='tensorboard_summary/', type=str, required=False, help='Tensorboard路径')
+    parser.add_argument('--segment', action='store_true', help='中文以词为单位')
+    parser.add_argument('--bpe_token', action='store_true', help='subword')
+    parser.add_argument('--encoder_json', default="tokenizations/encoder.json", type=str, help="encoder.json")
+    parser.add_argument('--vocab_bpe', default="tokenizations/vocab.bpe", type=str, help="vocab.bpe")
+    parser.add_argument('--max_steps_perEpoch_perPiece', default=1000000, type=int, required=False)
+    parser.add_argument('--seq_len', default=50, type=int, required=False)
+    parser.add_argument('--steps_savemodel', default=10000, type=int, required=False, help='保存模型步数')
+    args = parser.parse_args()
+    print('args:\n' + args.__repr__())
+
+    if args.segment:
+        from tokenizations import tokenization_bert_word_level as tokenization_bert
+    else:
+        from tokenizations import tokenization_bert
+
+    model_config = transformers.modeling_gpt2.GPT2Config.from_json_file(args.model_config)
+    print('config:\n' + model_config.to_json_string())
+
+    if args.bpe_token:
+        full_tokenizer = get_encoder(args.encoder_json, args.vocab_bpe)
+    else:
+        full_tokenizer = tokenization_bert.BertTokenizer(vocab_file=args.tokenizer_path)
+    full_tokenizer.max_len = 999999
+    raw_data_path = args.raw_data_path
+    tokenized_data_path = args.tokenized_data_path
+    log_step = args.log_step
+    gradient_accumulation = args.gradient_accumulation
+    num_pieces = args.num_pieces
+    min_length = args.min_length
+    seq_len = args.seq_len
+    assert log_step % gradient_accumulation == 0
+    print('building files')
+    build_files(data_path=raw_data_path, tokenized_data_path=tokenized_data_path, num_pieces=num_pieces,
+                full_tokenizer=full_tokenizer, min_length=min_length, seq_len=seq_len)
+    print('files built')
+
+if __name__ == '__main__':
+    main()
---
+++ getInputData/datapro_dabaigou.py
@@ -0,0 +1,66 @@
+import sys
+import os
+import shutil
+import tqdm
+from tokenizations import tokenization_bert
+import random
+def build_files(data_path, dataname, tokenized_data_path, full_tokenizer,n_ctx = 50, min_length=20,num_pieces=1, max_nb = 10000000):
+    if not os.path.exists(tokenized_data_path):
+        os.mkdir(tokenized_data_path)
+    k = 0
+    full_line = []
+    print('reading lines')
+    nb_lines = 0
+    for ii in range(5):
+        f = open(data_path+'/part-0000'+str(ii), 'r', encoding='utf8')
+        for line in f:
+            tmp = line.strip().split('\t')
+            if len(tmp)!=2:
+                continue
+            line = tmp[1]
+            if nb_lines%10000==0:
+                print('processing file %s, %d, %0.2f'%(data_path,nb_lines,nb_lines/float(max_nb)))
+            if len(line)<min_length:
+                continue
+            nb_lines += 1
+            subline = full_tokenizer.convert_tokens_to_ids(list(line))
+            #print(full_tokenizer.convert_tokens_to_ids('[MASK]'))
+            #print(subline)
+            #print(full_tokenizer.convert_tokens_to_ids('[CLS]'))
+            tmp = [full_tokenizer.convert_tokens_to_ids('[MASK]')]
+            tmp = tmp + subline
+            if len(tmp)>n_ctx-1:
+                tmp = tmp[:n_ctx-1]
+            else:
+                tmp = tmp+(n_ctx-1-len(tmp))*[full_tokenizer.convert_tokens_to_ids('[PAD]')]
+            tmp = tmp + [full_tokenizer.convert_tokens_to_ids('[CLS]')]
+            full_line.extend(tmp)
+            if nb_lines>=max_nb:
+                with open(tokenized_data_path + dataname+'-{}.txt'.format(k), 'w') as f:
+                    for id in full_line:
+                        f.write(str(id) + ' ')
+                k += 1
+                full_line = []
+                nb_lines = 0
+        f.close()
+    print('finish')
+def changenames():
+    path = './data/sogouInput_tokenized/'
+    filename_list = os.listdir(path)
+    for i in range(len(filename_list)):
+        file = filename_list[i]
+        oldpath = os.path.join(path,file)
+        newpath = oldpath[:len(path)]+'tokenized_train_{}.txt'.format(i)
+        os.rename(oldpath, newpath)
+        if i%100==0:
+            print(i,oldpath,newpath)
+
+def main(data_path,dataname):
+    tokenizer_path = '../model/gpt2_prose/vocab.txt'
+    tokenized_data_path = '../data/dabaigou_tokenized/'
+    full_tokenizer = tokenization_bert.BertTokenizer(vocab_file=tokenizer_path)
+    build_files(data_path, dataname, tokenized_data_path, full_tokenizer)
+    shutil.rmtree(data_path)
+if __name__=='__main__':
+    data_path,dataname = sys.argv[1:3]
+    main(data_path,dataname)
