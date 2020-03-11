450bf6fe8bc91a604126d43aeec69d3c60432948
baokui <sy880115>
2020-03-11 14:01:47 +0800

    get data

--- getInputData/run_removeUNK.sh
+++ getInputData/run_removeUNK.sh
@@ -1,8 +1,8 @@
 idx=47
-nohup python -u datapro_dabaigou_remvoveUNK.py idx >> log/remove-$idx.log 2>&1 &
+nohup python -u datapro_dabaigou_remvoveUNK.py $idx >> log/remove-$idx.log 2>&1 &
 idx=77
-nohup python -u datapro_dabaigou_remvoveUNK.py idx >> log/remove-$idx.log 2>&1 &
+nohup python -u datapro_dabaigou_remvoveUNK.py $idx >> log/remove-$idx.log 2>&1 &
 idx=107
-nohup python -u datapro_dabaigou_remvoveUNK.py idx >> log/remove-$idx.log 2>&1 &
+nohup python -u datapro_dabaigou_remvoveUNK.py $idx >> log/remove-$idx.log 2>&1 &
 idx=137
-nohup python -u datapro_dabaigou_remvoveUNK.py idx >> log/remove-$idx.log 2>&1 &
+nohup python -u datapro_dabaigou_remvoveUNK.py $idx >> log/remove-$idx.log 2>&1 &
