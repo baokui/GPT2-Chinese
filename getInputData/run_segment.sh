d7f7f4a9ec2c7ce243f25bcb992e2fec1c6b595f
baokui <sy880115>
2020-03-13 22:13:47 +0800

    get data

--- getInputData/run_segment.sh
+++ getInputData/run_segment.sh
@@ -2,5 +2,5 @@ mode=segment
 
 file=0000
 path_source=data/202002/$file
-path_source=data/202002_seg/$file.txt
+path_target=data/202002_seg/$file.txt
 nohup python -u sentsement.py $mode $path_source $path_target >> log/seg-$file.log 2>&1 &
