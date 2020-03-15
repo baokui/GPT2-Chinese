b73d12bed049e04767d29954d43d7d1ff6872de3
baokui <sy880115>
2020-03-15 16:39:56 +0800

    get data

--- getInputData/sentsement.py
+++ getInputData/sentsement.py
@@ -23,8 +23,44 @@ def segment(path_source,path_target):
                 print('sengment file %s with %d lines'%(file,n))
     if len(S)>0:
         f_w.write(''.join(S))
+def getVocab(path_source,path_target,filname,max_line=10000000):
+    if not os.path.exists(path_target):
+        os.mkdir(path_target)
+    f = open(path_source,'r')
+    N = 0
+    n = -1
+    D = {}
+    S = []
+    for line in f:
+        n += 1
+        data = line.strip().split('\t')
+        if len(data)!=2:
+            continue
+        s = data[1].split(' ')
+        for ss in s:
+            if ss not in D:
+                D[ss] = 1
+            else:
+                D[ss] += 1
+        if n>0 and n%max_line==0:
+            S = [[k,str(D[k])] for k in D]
+            S = sorted(S,key=lambda x:x[0])
+            S = ['\t'.join(tmp) for tmp in S]
+            with open(os.path.join(path_target,filname+'_'+str(N)+'.txt'),'w') as f_w:
+                f_w.write('\n'.join(S))
+            print('complete %d lines for %s'%(n,path_source))
+            N += 1
+            D = {}
+
+    if len(S)>0:
+        with open(os.path.join(path_target, filname + '_' + str(N) + '.txt'), 'w') as f_w:
+            f_w.write('\n'.join(S))
+    f.close()
 if __name__=="__main__":
     mode = sys.argv[1]
     if mode=='segment':
         path_source,path_target = sys.argv[2:4]
         segment(path_source,path_target)
+    if mode=='vocab':
+        path_source, path_target,filename = sys.argv[2:5]
+        getVocab(path_source,path_target,filename)
