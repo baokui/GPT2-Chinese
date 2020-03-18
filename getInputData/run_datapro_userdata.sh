b47573d47869345443ed56798353d79973706440
baokui <sy880115>
2020-03-18 14:15:32 +0800

    get data

--- getInputData/run_datapro_userdata.sh
+++ getInputData/run_datapro_userdata.sh
@@ -1,18 +1,16 @@
 mode=char
 path_data=../data/userdata
-idx=00
-name=$idx.txt
-nohup python -u datapro_userdata.py $mode $path_data $idx $name >> log/datapro-userdata-$name.log 2>&1 &
 
-
-for((j=0;j<4;j++))
+for((j=0;j<10;j++))
 do
-for((i=0;i<9;i++))
-do
-name=00$j$i
-nohup python -u datapro_dabaigou.py $mode ../data/$filename/$name.txt $name >> log/datapro-dabaigou-seg-$name.log 2>&1 &
-done
-i=9
-name=00$j$i
-nohup python -u datapro_dabaigou.py $mode ../data/$filename/$name.txt $name >> log/datapro-dabaigou-seg-$name.log
+    for((i=0;i<9;i++))
+    do
+        idx=$j$i
+        name=$idx.txt
+        nohup python -u datapro_userdata.py $mode $path_data $idx $name >> log/datapro-userdata-$name.log 2>&1 &
+    done
+    i=9
+    idx=$j$i
+    name=$idx.txt
+    nohup python -u datapro_userdata.py $mode $path_data $idx $name >> log/datapro-userdata-$name.log 2>&1
 done
