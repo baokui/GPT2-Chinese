6629bf7f3d1382b419780d5e58bee23c859de2be
baokui <sy880115>
2020-03-27 18:19:16 +0800

    gpt2-generating

--- web/run_app.sh
+++ web/run_app.sh
@@ -1,6 +1,8 @@
 port=5000
 nohup /root/anaconda3/envs/pytorch/bin/python3.6 app.py $port >> log/$port.log 2>&1 &
+port=5002
+nohup /root/anaconda3/envs/pytorch/bin/python3.6 app.py $port >> log/$port.log 2>&1 &
 port=5001
-nohup /root/anaconda3/envs/pytorch/bin/python3.6 app_nnlm.py $port >> log/$port.log 2>&1 &
+#nohup /root/anaconda3/envs/pytorch/bin/python3.6 app_nnlm.py $port >> log/$port.log 2>&1 &
 #port=5002
 #nohup python app.py $port >> log/$port.log 2>&1 &
