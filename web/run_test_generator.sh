3b97205a291b51130f8ab8eab4d1a3720467ed9f
baokui <sy880115>
2020-03-31 15:04:59 +0800

    gpt2-generating

--- web/run_test_generator.sh
+++ web/run_test_generator.sh
@@ -1,10 +1,11 @@
 path_source=../cgi-bin/data/input3.txt
 mode=0
-CUDA_VISIBLE_DEVICES=1
+CUDA_VISIBLE_DEVICES=7
 
 path_config=config/config_godText_large1.json
 path_target=final
-nohup python test_generator.py $mode $path_config $path_source result/$path_target.json >> log/test-gen-$path_target.log 2>&1 &
+topk=10
+nohup python test_generator.py $mode $path_config $path_source result/$path_target.json $topk >> log/test-gen-$path_target-$topk.log 2>&1 &
 
 path_config=config/config_godText_large1_17.json
 path_target=model_epoch17
