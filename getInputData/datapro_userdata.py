140f3598171565d784f4f004624bc0c7135acee2
baokui <sy880115>
2020-03-31 15:56:17 +0800

    gpt2-generating

--- getInputData/datapro_userdata.py
+++ getInputData/datapro_userdata.py
@@ -45,7 +45,7 @@ def build_files(data_path, dataname, tokenized_data_path, full_tokenizer, idx, n
                 tmp,line = token_pad(full_tokenizer,subline,n_ctx)
                 full_line.extend(tmp)
                 while len(line)>n_ctx:
-                    tmp, line = token_pad(full_tokenizer, subline, n_ctx)
+                    tmp, line = token_pad(line,full_tokenizer,subline,n_ctx)
                     full_line.extend(tmp)
             else:
                 tmp = [full_tokenizer.convert_tokens_to_ids('[MASK]')]
