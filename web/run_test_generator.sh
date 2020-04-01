2f8656e3dcddfcb4f8951f1011d11be3a1f37cc5
baokui <sy880115>
2020-04-01 11:50:43 +0800

    gpt2-generating

--- web/run_test_generator.sh
+++ web/run_test_generator.sh
@@ -4,23 +4,23 @@ mode=0
 path_config=demo_config/config_godText_large1.json
 path_target=final
 topk=10
-temp=1.0
+temp=0.7
 nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp.json $topk $temp >> log/test-gen-$path_target-$topk-$temp.log 2>&1 &
 
-topk=10
-temp=0.2
+topk=8
+temp=0.7
 nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp.json $topk $temp >> log/test-gen-$path_target-$topk-$temp.log 2>&1 &
 
 
-topk=10
-temp=0.2
+topk=6
+temp=0.7
 nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp-1.json $topk $temp >> log/test-gen-$path_target-$topk-$temp-1.log 2>&1 &
 
-topk=10
-temp=0.5
+topk=4
+temp=0.7
 nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp.json $topk $temp >> log/test-gen-$path_target-$topk-$temp.log 2>&1 &
 
 
-topk=5
-temp=0.5
+topk=10
+temp=0.8
 nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp.json $topk $temp >> log/test-gen-$path_target-$topk-$temp.log 2>&1 &
