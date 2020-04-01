5d933ac8d00aa85b804568801174e626852b6eba
baokui <sy880115>
2020-04-01 11:52:21 +0800

    gpt2-generating

--- web/run_test_generator.sh
+++ web/run_test_generator.sh
@@ -4,23 +4,23 @@ mode=0
 path_config=demo_config/config_godText_large1.json
 path_target=final
 topk=10
-temp=0.7
-nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp.json $topk $temp >> log/test-gen-$path_target-$topk-$temp.log 2>&1 &
-
-topk=8
-temp=0.7
-nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp.json $topk $temp >> log/test-gen-$path_target-$topk-$temp.log 2>&1 &
-
-
-topk=6
-temp=0.7
-nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp-1.json $topk $temp >> log/test-gen-$path_target-$topk-$temp-1.log 2>&1 &
-
-topk=4
-temp=0.7
-nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp.json $topk $temp >> log/test-gen-$path_target-$topk-$temp.log 2>&1 &
-
+temp=0.9
+nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp.json $topk $temp >> log/test-gen-$path_target-$topk-$temp-$mode.log 2>&1 &
 
 topk=10
 temp=0.8
-nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp.json $topk $temp >> log/test-gen-$path_target-$topk-$temp.log 2>&1 &
+nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp.json $topk $temp >> log/test-gen-$path_target-$topk-$temp-$mode.log 2>&1 &
+
+
+topk=10
+temp=0.7
+nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp.json $topk $temp >> log/test-gen-$path_target-$topk-$temp-$mode.log 2>&1 &
+
+topk=10
+temp=0.6
+nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp.json $topk $temp >> log/test-gen-$path_target-$topk-$temp-$mode.log 2>&1 &
+
+
+topk=10
+temp=0.5
+nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp.json $topk $temp >> log/test-gen-$path_target-$topk-$temp-$mode.log 2>&1 &
