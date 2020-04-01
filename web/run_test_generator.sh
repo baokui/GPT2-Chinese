c0a4b3ac9d4de4931111a3f7ea2dd5129a64bf33
baokui <sy880115>
2020-04-01 11:28:51 +0800

    gpt2-generating

--- web/run_test_generator.sh
+++ web/run_test_generator.sh
@@ -1,12 +1,12 @@
 path_source=result/input3.txt
 mode=0
-#CUDA_VISIBLE_DEVICES=7
 
-path_config=config/config_godText_large1.json
+path_config=demo_config/config_godText_large1.json
 path_target=final
 topk=10
-nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk.json $topk >> log/test-gen-$path_target-$topk.log 2>&1 &
+temp=1.0
+nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp.json $topk $temp >> log/test-gen-$path_target-$topk.log 2>&1 &
 
-path_config=config/config_godText_large1_17.json
-path_target=model_epoch17
-#nohup python test_generator.py $mode $path_config $path_source result/$path_target.json >> log/test-gen-$path_target.log 2>&1 &
+topk=10
+temp=0.2
+nohup python -u test_generator.py $mode $path_config $path_source result/$path_target-$topk-$temp.json $topk $temp >> log/test-gen-$path_target-$topk.log 2>&1 &
--- web/test_generator.py
+++ web/test_generator.py
@@ -6,25 +6,24 @@ from datetime import datetime
 import time
 import logging
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "7"
-batchGenerating=True
-path_HFW = '../data/words_highFreq.txt'
-path_configs = ['config/config_godText_large1.json','config/config_poem.json','config/config_dabaigou.json']
-num0 = [20,4,4]
-tags = ['(文)','(诗)','(大白狗)','(句联想)']
-rmHFW = [False,False,True,False]
-maxNext = 3
-path_next = 'model/nnlm/D_next.json'
-path_simi = 'model/nnlm/D_simi.json'
+from Config import config_predict
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+ConfigPredict = config_predict()
+batchGenerating=ConfigPredict.batchGenerating
+path_configs = ConfigPredict.path_configs
+num0 = ConfigPredict.predict_nums
+tags = ConfigPredict.tags
+rmHFW = ConfigPredict.rmHFW
+maxNext = ConfigPredict.maxNext_JLX
+path_next = ConfigPredict.path_JLX_next
+path_simi = ConfigPredict.path_JLX_simi
 quick = False
-HFW = [[],[],[],[]]
-with open(path_HFW,'r') as f:
-    HFW[2] = f.read().strip().split('\n')
-
-def main(path_data,mode,path_config,path_target,topk):
+app = None
+def main(path_data,mode,path_config,path_target,topk,temp):
     ii = int(mode)
     model, tokenizer, config, device = gpt_gen.getModel(path_config=path_config)
     config['topk'] = topk
+    config['temperature'] = temp
     with open(path_data,'r') as f:
         s = f.read().strip().split('\n')
     D = []
@@ -32,10 +31,13 @@ def main(path_data,mode,path_config,path_target,topk):
     for data in s:
         result = []
         for _ in range(3):
-            if ii==1:
-                r0 = gpt_gen.generating_poem('a',data, model, config, tokenizer,device,quick,num0[ii],batchGenerating=batchGenerating)
+            if ii == 1:
+                r0 = gpt_gen.generating_poem(app, data, model, config, tokenizer, device, quick,
+                                             num0[ii], batchGenerating=batchGenerating)
             else:
-                r0 = gpt_gen.generating('a',data, model, config, tokenizer,device,quick,num0[ii],removeHighFreqWords=rmHFW[ii],HighFreqWords=HFW[ii],batchGenerating=batchGenerating)
+                r0 = gpt_gen.generating(app, data, model, config, tokenizer, device, ConfigPredict,
+                                        quick=quick, num=num0[ii], removeHighFreqWords=rmHFW[ii],
+                                        batchGenerating=batchGenerating)
             r0 = [rr + tags[ii] for rr in r0]
             result.extend(r0)
         d = {'input':data,'outputs':result,'num':len(result)}
@@ -46,8 +48,10 @@ def main(path_data,mode,path_config,path_target,topk):
     print('predict time is {} for parameter topk={}'.format(t1-t0,topk))
 if __name__=='__main__':
     mode,path_config,data,path_target = sys.argv[1:5]
-    if len(sys.argv)>5:
+    if len(sys.argv)>6:
         topk = int(sys.argv[5])
+        temp = float(sys.argv[6])
     else:
         topk = 8
-    main(data,mode,path_config,path_target,topk)
+        temp = 1.0
+    main(data,mode,path_config,path_target,topk,temp)
