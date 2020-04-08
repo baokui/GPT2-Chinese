a361a26bbf9db63ab06da893a134d0b33dd98f54
baokui <sy880115>
2020-04-08 20:22:51 +0800

    gpt2-peom generating

--- peom_generating/run_peomGenerator.sh
+++ peom_generating/run_peomGenerator.sh
@@ -13,17 +13,17 @@ idx1=20000
 gpu=1
 nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=10000
-idx1=20000
-gpu=2
-nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
-
 idx0=20000
 idx1=30000
-gpu=3
+gpu=2
 nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
 idx0=30000
 idx1=40000
+gpu=3
+nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
+
+idx0=40000
+idx1=50000
 gpu=4
 nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
