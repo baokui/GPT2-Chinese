7b4e6c88663f92eec3b44e11b384652568ce9317
baokui <sy880115>
2020-04-08 19:28:11 +0800

    gpt2-generating

--- cgi-bin/run_testing.sh
+++ cgi-bin/run_testing.sh
@@ -5,7 +5,7 @@ mode=noseg
 fp16=1
 modeType=dabaigou
 path_target=result/$name-$fp16.json
-#nohup python -u testing.py $path_config $mode $path_source $path_target $fp16 $modeType >> log/testing-$name.log 2>&1 &
+nohup python -u testing.py $path_config $mode $path_source $path_target $fp16 $modeType >> log/testing-$name.log 2>&1 &
 '''
 name=dabaigou
 path_config=config/config_$name.json
