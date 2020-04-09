688d5c788fc82612238171b1e08a12bf0eec42bc
baokui <sy880115>
2020-04-09 22:07:48 +0800

    gpt2-peom generating

--- peom_generating/run_peomGenerator.sh
+++ peom_generating/run_peomGenerator.sh
@@ -4,42 +4,42 @@ path_source=../data/poetry_small.json
 nsamples=15
 path_config=../web/demo_config/config_poem.json
 
-idx0=80000
-idx1=90000
+idx0=160000
+idx1=170000
 gpu=0
 nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=90000
-idx1=100000
+idx0=170000
+idx1=180000
 gpu=1
 nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=100000
-idx1=110000
+idx0=180000
+idx1=190000
 gpu=2
 nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=110000
-idx1=120000
+idx0=190000
+idx1=200000
 gpu=3
 nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=120000
-idx1=130000
-gpu=0
+idx0=200000
+idx1=210000
+gpu=4
 nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=130000
-idx1=140000
-gpu=1
+idx0=210000
+idx1=220000
+gpu=5
 nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=140000
-idx1=150000
-gpu=2
+idx0=220000
+idx1=230000
+gpu=6
 nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=150000
-idx1=160000
-gpu=3
+idx0=230000
+idx1=240000
+gpu=7
 nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
