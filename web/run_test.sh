100368c9c9a4be2a0fea1c7851562e2c172755cf
baokui <sy880115>
2020-04-09 11:17:48 +0800

    gpt2-peom generating

---
+++ web/run_test.sh
@@ -0,0 +1,12 @@
+path_source=data/test_text.txt
+
+penalty=1.2
+path_target=result/test_text-penalty=$penalty.json
+gpus=0,1,2
+nohup python -u test.py $path_source $path_target $gpus $penalty >> log/test_text-penalty=$penalty.log 2>&1 &
+
+penalty=1.5
+path_target=result/test_text-penalty=$penalty.json
+gpus=3,4,5
+nohup python -u test.py $path_source $path_target $gpus $penalty >> log/test_text-penalty=$penalty.log 2>&1 &
+
