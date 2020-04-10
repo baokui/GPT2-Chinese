a6bc463005a31b7e00aa25242d2538ced5ade7ce
baokui <sy880115>
2020-04-10 15:41:00 +0800

    gpt2-peom generating

--- peom_generating/peomGenerator.py
+++ peom_generating/peomGenerator.py
@@ -26,32 +26,22 @@ def main(path_source,path_config,idx0,idx1,gpu,nsamples):
     path_target = 'data/'+str(idx0)+'_'+str(idx1)
     if not os.path.exists(path_target):
         os.mkdir(path_target)
-    D_prefix = {}
     S = data[idx0:idx1]
-    R = {}
+    N = 0
+    n = 0
     for i in range(len(S)):
-        if i%2==0:
-            n = sum([len(R[tt]) for tt in R])
-            print("proceed {} poem (total {}), get {} files and generate {} poems".format(i,len(S),len(D_prefix),n))
+        if i%10==0:
+            print("proceed {} poem (total {}), get {} prefix and generate {} poems".format(i,len(S),n,N))
         sents = peomSplit(S[i])
         for s in sents:
+            n+=1
             r = generating(s, model, config, tokenizer, device, nsamples, gpu)
+            N+=len(r)
+            if len(r)==0:
+                continue
             r = [s+'\t'+rr[len(s):] for rr in r]
-            if s[0] in D_prefix:
-                D_prefix[s[0]]+=1
-                R[s[0]].extend(r)
-            else:
-                D_prefix[s[0]] = 1
-                R[s[0]] = r
-            if len(R[s[0]])%10==0:
-                with open(path_target+'/'+s[0]+'.txt','a+') as f:
-                    f.write('\n'.join(R[s[0]])+'\n')
-                R[s[0]] = []
-    for s in D_prefix:
-        if len(R[s])>0:
-            with open(path_target + '/' + s + '.txt', 'a+') as f:
-                f.write('\n'.join(R[s]) + '\n')
-        R[s] = []
+            with open(path_target+'/'+s[0]+'.txt','a+') as f:
+                f.write('\n'.join(r)+'\n')
 if __name__=='__main__':
     path_source, path_config, idx0, idx1, gpu, nsamples = sys.argv[1:]
     idx0 = int(idx0)
