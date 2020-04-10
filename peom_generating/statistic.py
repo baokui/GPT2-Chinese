0f88fb8c66f99318085729ba2b344db82bc0a309
baokui <sy880115>
2020-04-10 12:24:51 +0800

    gpt2-peom generating

---
+++ peom_generating/statistic.py
@@ -0,0 +1,23 @@
+import json
+from peomGenerator import peomSplit
+import sys
+def getStats(path_source):
+    with open(path_source, 'r') as f:
+        data = json.load(f)
+    n = 5
+    N = len(data)
+    N_less5_head = 0
+    N_less5_sent = 0
+    for i in range(N):
+        if i%10000==0:
+            print('number of all/head less than 5 chars/sent less than 5 chars is %d/%d%d'%(N,N_less5_head,N_less5_sent))
+        S = peomSplit(data[i])
+        if len(S[0])<n:
+            N_less5_head += 1
+        for s in S:
+            if len(s)<n:
+                N_less5_sent += 1
+    print('number of all/head less than 5 chars/sent less than 5 chars is %d/%d%d' % (N, N_less5_head, N_less5_sent))
+if __name__=='__main__':
+    path_sourc = sys.argv[1]
+    getStats(path_sourc)
