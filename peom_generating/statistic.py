419aab18e50a12c902e7f6be9269126029336e8e
baokui <sy880115>
2020-04-10 12:30:43 +0800

    gpt2-peom generating

--- peom_generating/statistic.py
+++ peom_generating/statistic.py
@@ -18,6 +18,10 @@ def getStats(path_source):
             if len(s)<n:
                 N_less5_sent += 1
     print('number of all/head less than 5 chars/sent less than 5 chars is %d/%d/%d' % (N, N_less5_head, N_less5_sent))
+    print('全部诗词：%d'%N)
+    print('少于5个字的诗句：%d'%N_less5_sent)
+    print('少于5个字的首句：%d'%N_less5_head)
+    
 if __name__=='__main__':
     path_sourc = sys.argv[1]
     getStats(path_sourc)
