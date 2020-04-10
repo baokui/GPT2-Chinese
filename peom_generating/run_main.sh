c672e8c1f71cf517a81bc5b9e92522212e85a68d
baokui <sy880115>
2020-04-10 22:48:47 +0800

    gpt2-peom generating

--- peom_generating/run_main.sh
+++ peom_generating/run_main.sh
@@ -1,39 +1,39 @@
-idx0=0
-idx1=10000
+idx0=160000
+idx1=170000
 gpu=0
 nohup sh run_each.sh $idx0 $idx1 $gpu >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=10000
-idx1=20000
+idx0=170000
+idx1=180000
 gpu=1
 nohup sh run_each.sh $idx0 $idx1 $gpu >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=20000
-idx1=30000
+idx0=180000
+idx1=190000
 gpu=2
 nohup sh run_each.sh $idx0 $idx1 $gpu >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=30000
-idx1=40000
+idx0=190000
+idx1=200000
 gpu=3
 nohup sh run_each.sh $idx0 $idx1 $gpu >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=40000
-idx1=50000
+idx0=200000
+idx1=210000
 gpu=4
 nohup sh run_each.sh $idx0 $idx1 $gpu >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=50000
-idx1=60000
+idx0=210000
+idx1=220000
 gpu=5
 nohup sh run_each.sh $idx0 $idx1 $gpu >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=60000
-idx1=70000
+idx0=220000
+idx1=230000
 gpu=6
 nohup sh run_each.sh $idx0 $idx1 $gpu >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=70000
-idx1=80000
+idx0=230000
+idx1=240000
 gpu=7
 nohup sh run_each.sh $idx0 $idx1 $gpu >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
