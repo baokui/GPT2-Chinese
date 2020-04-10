56dd320d0ad64b7278f056078338767e9cb52ee1
baokui <sy880115>
2020-04-10 12:32:41 +0800

    gpt2-peom generating

--- peom_generating/statistic.py
+++ peom_generating/statistic.py
@@ -6,12 +6,14 @@ def getStats(path_source):
         data = json.load(f)
     n = 5
     N = len(data)
+    N_all = 0
     N_less5_head = 0
     N_less5_sent = 0
     for i in range(N):
         if i%10000==0:
             print('number of all/head less than 5 chars/sent less than 5 chars is %d/%d/%d'%(N,N_less5_head,N_less5_sent))
         S = peomSplit(data[i])
+        N_all+=len(S)
         if len(S[0])<n:
             N_less5_head += 1
         for s in S:
@@ -19,9 +21,10 @@ def getStats(path_source):
                 N_less5_sent += 1
     print('number of all/head less than 5 chars/sent less than 5 chars is %d/%d/%d' % (N, N_less5_head, N_less5_sent))
     print('全部诗词：%d'%N)
+    print('全部诗词的诗句：%d'%N_all)
     print('少于5个字的诗句：%d'%N_less5_sent)
     print('少于5个字的首句：%d'%N_less5_head)
-    
+
 if __name__=='__main__':
     path_sourc = sys.argv[1]
     getStats(path_sourc)
