379afaa33d6b9d3b4bdf50db372cd456ef900e1c
baokui <sy880115>
2020-04-10 16:33:09 +0800

    gpt2-peom generating

--- peom_generating/run_each.sh
+++ peom_generating/run_each.sh
@@ -7,9 +7,10 @@ path_source=../data/poetry_small.json
 nsamples=15
 path_config=../web/demo_config/config_poem.json
 i0=$idx0
-while [ $i0 -le $idx1 ]
+while [ $i0 -lt $idx1 ]
 do
-echo process $i0-$(( $i0 + $batch ))
-#nohup python -u peomGenerator.py $path_source $path_config $i0 $(($i0+$batch)) $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1
-i0=$(( $i0 + $batch ))
+i1=$(( $i0 + $batch ))
+echo process $i0-$i1
+#nohup python -u peomGenerator.py $path_source $path_config $i0 $i1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1
+i0=$i1
 done
