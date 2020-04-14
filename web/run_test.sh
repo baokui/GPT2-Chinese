adeefec1c960528db611236a3901889a59c233fe
baokui <sy880115>
2020-04-14 14:55:57 +0800

    gpt2-peom generating

--- web/run_test.sh
+++ web/run_test.sh
@@ -1,4 +1,4 @@
 path_source=data/test_text.txt
 path_target=result/test_text-resort-20-smallmodel.json
 path_config=demo_config/config_godText_large1.json,demo_config/config_poem.json,demo_config/config_dabaigou_small.json
-nohup python -u test.py $path_source $path_target $path_config >> log/test_text.log 2>&1 &
+nohup python -u test.py $path_source $path_target $path_config >> log/test_text-small.log 2>&1 &
