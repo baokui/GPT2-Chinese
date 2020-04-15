b7dafc2fc32a8dd8c1012d64c0090995d495f323
baokui <sy880115>
2020-04-15 19:13:29 +0800

    gpt2-peom generating

--- getInputData/run_datapro_godText.sh
+++ getInputData/run_datapro_godText.sh
@@ -5,4 +5,4 @@ padding=1
 path_vocab=../model/model_godText_pretrain/vocab.txt
 nb_piece=20
 n_ctx=64
-nohup python -u datapro_godText.py $path_source $path_target $padding >> log/godText-padding.log 2>&1 &
+nohup python -u datapro_godText.py $path_source $path_target $padding $path_vocab $nb_piece $n_ctx >> log/godText-padding.log 2>&1 &
