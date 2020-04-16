028ac6a87fe4f503c510365e692436d0580b3f27
baokui <sy880115>
2020-04-16 16:51:18 +0800

    gpt2-peom generating

--- web/run_test.sh
+++ web/run_test.sh
@@ -1,5 +1,5 @@
 path_source=data/test_text.txt
 path_target=result/test_text-godTextPretrain.json
-path_config=demo_config/config_godText_pretrain.json,demo_config/config_godText_pretrain.json,demo_config/config_godText_pretrain.json
-doPredict=1,0,0,0
+path_config=demo_config/config_godText_pretrain1.json,demo_config/config_godText_large1.json,demo_config/config_godText_pretrain.json
+doPredict=1,1,0,0
 nohup python -u test.py $path_source $path_target $path_config $doPredict >> log/test_text-test.log 2>&1 &
