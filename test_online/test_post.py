ccf3d9cf166ed222c866e9d6328eb0fc8cb298fc
baokui <sy880115>
2020-04-19 16:27:41 +0800

    gpt2-peom generating

--- test_online/test_post.py
+++ test_online/test_post.py
@@ -9,5 +9,7 @@ def main():
     for str in s:
         data = {"input":str}
         res = requests.post(url=url,json=data)
+        if res.json()['message']!='success':
+            print(res.json())
     t1 = time.time()
     print('number of samles:{},total time:{}s, QPS:{}'.format(len(s),'%0.4f'%(t1-t0),'%0.4f'%(len(s)/(t1-t0))))
