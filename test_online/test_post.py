a99793ed011bfabccb2fc8cbb53c7341690e7d75
baokui <sy880115>
2020-04-19 16:31:36 +0800

    gpt2-peom generating

--- test_online/test_post.py
+++ test_online/test_post.py
@@ -13,3 +13,5 @@ def main():
             print(res.json())
     t1 = time.time()
     print('number of samles:{},total time:{}s, QPS:{}'.format(len(s),'%0.4f'%(t1-t0),'%0.4f'%(len(s)/(t1-t0))))
+if __name__=='__main__':
+    main()
