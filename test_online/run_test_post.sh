7cfa757fd1ea000364a7afe9896dc3374c8198dc
baokui <sy880115>
2020-04-19 22:33:47 +0800

    gpt2-peom generating

--- test_online/run_test_post.sh
+++ test_online/run_test_post.sh
@@ -3,5 +3,5 @@ for((i=0;i<$N;i++))
 do
 ii=$[$i%10]
 port=$[ii+5000]
-nohup python -u test_post.py $port >> log/test-$N-$port.log 2>&1 &
+nohup python -u test_post.py $port >> log/test-$N-1-$i.log 2>&1 &
 done
