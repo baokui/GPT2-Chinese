9d778be29470eda26b31a484a8ee1a56b4d426a4
baokui <sy880115>
2020-04-20 13:16:12 +0800

    gpt2-peom generating

--- demo_train/run_test.sh
+++ demo_train/run_test.sh
@@ -1,6 +1,6 @@
-path_source=data/test_cut.txt
+path_source=data/test_text.txt
 path_target=result/test_text-godTextPretrain.json
-path_config=demo_config/config_dabaigou_new.json,demo_config/config_dabaigou.json,demo_config/config_godText_pretrain.json
-doPredict=1,1,0,0
-gpus=0,1
+path_config=demo_config/config_dabaigou_new.json,demo_config/config_dabaigou.json,demo_config/config_dabaigou_new.json
+doPredict=0,1,1,0
+gpus=0,1,2
 nohup python -u test.py $path_source $path_target $path_config $doPredict $gpus >> log/test_text-test.log 2>&1 &
