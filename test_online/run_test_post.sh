cd0917ad40be0995a3d595f3ba3794b1057fa1d4
baokui <sy880115>
2020-04-20 11:36:51 +0800

    gpt2-peom generating

--- test_online/run_test_post.sh
+++ test_online/run_test_post.sh
@@ -1,8 +1,9 @@
 N=$1
 maxPort=$2
+name=$3
 for((i=0;i<$N;i++))
 do
 ii=$[$i%$maxPort]
 port=$[ii+5000]
-nohup python -u test_post.py $port >> log/test-$N-1-$i.log 2>&1 &
+nohup python -u test_post.py $port >> log/test-$name-$maxPort-$N.log 2>&1 &
 done
