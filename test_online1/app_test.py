1a57bb75c1ed2b26061cd1a7c914268dcee38c97
baokui <sy880115>
2020-04-21 17:41:29 +0800

    gpt2-peom generating

---
+++ test_online1/app_test.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from  gevent.pywsgi import WSGIServer
+from gevent import monkey
+import time
+monkey.patch_all()
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    year = time.localtime().tm_year
+    month = time.localtime().tm_mon
+    day = time.localtime().tm_mday
+
+    hour = time.localtime().tm_hour
+    minute = time.localtime().tm_min
+    second = time.localtime().tm_sec
+
+    T0 = '-'.join([str(hour), '时', str(minute), '分', str(second), '秒'])
+    time.sleep(10)
+
+    hour = time.localtime().tm_hour
+    minute = time.localtime().tm_min
+    second = time.localtime().tm_sec
+
+    T1 = '-'.join([str(hour), '时', str(minute), '分', str(second), '秒'])
+    return 'Hello World!'+T0+'->'+T1
+
+@app.route('/index')
+def beijing():
+    return 'Beijing'
+if __name__ == '__main__':
+    http_server = WSGIServer(('127.0.0.1', 5001), app)
+    http_server.serve_forever()
