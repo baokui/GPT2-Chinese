986591e61639ca2cdf0c9893808de9b8fa14556f
baokui <sy880115>
2020-04-21 20:41:44 +0800

    gpt2-peom generating

--- test_online1/run_app_test.sh
+++ test_online1/run_app_test.sh
@@ -1,7 +1,7 @@
 mode=$1
 if [ $mode -eq 1 ]
 then
-    ps -ef|grep app_test|grep -v grep|awk  '{print "kill -9 " $2}' |sh
+    #ps -ef|grep app_test|grep -v grep|awk  '{print "kill -9 " $2}' |sh
     nohup python -u app_test.py 6000 >> log/apptest-6000.log 2>&1 &
 else
     for((i=0;i<10;i++))
