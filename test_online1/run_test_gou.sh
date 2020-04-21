c11f774925a431f18423bca41e70282ff939877e
baokui <sy880115>
2020-04-21 16:56:11 +0800

    gpt2-peom generating

--- test_online1/run_test_gou.sh
+++ test_online1/run_test_gou.sh
@@ -4,5 +4,5 @@ url0=http://10.160.22.73:
 port=5000
 for((i=0;i<$N;i++))
 do
-nohup python -u test_post_online.py $port $url0 >> log/test-gou-$maxThread-$N.log 2>&1 &
+nohup python -u test_gou.py $port $url0 >> log/test-gou-$maxThread-$N.log 2>&1 &
 done
