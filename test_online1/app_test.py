2cb405b8f591ba2e118a4894bc172ca2c2cd82d1
baokui <sy880115>
2020-04-21 17:52:37 +0800

    gpt2-peom generating

--- test_online1/app_test.py
+++ test_online1/app_test.py
@@ -3,8 +3,26 @@ from  gevent.pywsgi import WSGIServer
 from gevent import monkey
 import time
 import sys
-port = int(sys.argv[1])
+import os
+
+import gpt_gen
+from Config_gou import config_predict
+
+port = 6000
+if len(sys.argv)>1:
+   port = int(sys.argv[1])
 monkey.patch_all()
+
+ConfigPredict = config_predict()
+batchGenerating=ConfigPredict.batchGenerating
+path_configs = ConfigPredict.model_configs
+num0 = ConfigPredict.predict_nums
+tags = ConfigPredict.tags
+rmHFW = ConfigPredict.rmHFW
+gpus = ConfigPredict.gpus
+os.environ["CUDA_VISIBLE_DEVICES"]=gpus
+model,tokenizer,config,device = gpt_gen.getModel(path_config=path_configs,gpu=ConfigPredict.gpus)
+
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
