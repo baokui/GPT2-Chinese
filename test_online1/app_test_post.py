c054bf48c76862d5dfba3f497317921e29ef7cb8
baokui <sy880115>
2020-04-22 23:23:04 +0800

    gpt2-peom generating

--- test_online1/app_test_post.py
+++ test_online1/app_test_post.py
@@ -8,11 +8,15 @@ def main_gen(path_data,path_target,path_log):
         Data = f.read().strip().split('\n')
     R = []
     f = open(path_log,'a+')
+    TT0 = 0
     for data in Data:
         idx = random.randint(0,10)
         user_info = {"input":data,"idx":idx}
         T0 = time.asctime(time.localtime(time.time()))[11:19]
+        z0 = time.time()
         r = requests.post("http://127.0.0.1:7000/api/gen_test", data=user_info)
+        z1 = time.time()
+        TT0 = TT0+z1-z0
         T1 = time.asctime(time.localtime(time.time()))[11:19]
         s = r.text.split('\t')
         R.append(json.loads(s[0]))
@@ -20,6 +24,9 @@ def main_gen(path_data,path_target,path_log):
         f.write('outer:'+'\t'+T0+'\t'+T1+'\t'+'inter:' + '\t' + t0 + '\t' + t1 + '\n')
     with open(path_target,'w',encoding='utf-8') as f:
         json.dump(R,f,ensure_ascii=False,indent=4)
+    s0 = '%0.4f'%TT0
+    s1 = '%0.4f'%(len(Data)/TT0)
+    print(path_log+': total samples:{}, total time:{}s, qps:{}'.format(len(Data),s0,s1))
 def main_test(path_data,path_target,path_log):
     for i in range(10):
         idx = random.randint(0,10)
