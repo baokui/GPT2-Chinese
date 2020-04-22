2074024a2899f5d8ffcc7507ed5583c2a2b008f0
baokui <sy880115>
2020-04-22 17:09:21 +0800

    gpt2-peom generating

--- test_online1/run_app_test.sh
+++ test_online1/run_app_test.sh
@@ -1,6 +1,7 @@
 mode=$1
 gpus=$2
 N=$3
+time=$(date "+%Y-%m-%d--%H:%M:%S")
 if [ $mode -eq 1 ]
 then
     #ps -ef|grep app_test|grep -v grep|awk  '{print "kill -9 " $2}' |sh
@@ -16,7 +17,7 @@ else
         #echo http://127.0.0.1:7000/api/gen_test -d $param -X POST
         path_data=data/test.txt
         path_target=result/tmp-$i.json
-        path_log=log/apptest-post-$N-$i.log
+        path_log=log/apptest-post-$time.log
         nohup python -u app_test_post.py $path_data $path_target $path_log >> log/tmp.log 2>&1 &
     done
 fi
