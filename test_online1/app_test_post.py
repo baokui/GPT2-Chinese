c93d0614a87b4b1bb197765d703f66802a0578ce
baokui <sy880115>
2020-04-22 15:40:09 +0800

    gpt2-peom generating

---
+++ test_online1/app_test_post.py
@@ -0,0 +1,9 @@
+import requests
+import time
+def main(data):
+    user_info = {"input":data}
+    T0 = time.asctime(time.localtime(time.time()))
+    r = requests.post("http://127.0.0.1:7000/api/gen_test", data=user_info)
+    T1 = time.asctime(time.localtime(time.time()))
+    s = r.text+'\t'+'post-time:'+T0[11:19]+'->'+T1[11:19]
+    print(s)
