195ff5e1b9246bea4858d8ea8e2fba3fe49df0e9
baokui <sy880115>
2020-05-01 16:31:54 +0800

    gpt2-peom generating

--- test/test_compare.py
+++ test/test_compare.py
@@ -3,39 +3,7 @@ import json
 import gpt_gen
 import gpt_gen_thread
 import sys
-
-nb_models = sys.argv[1]
-ConfigPredict = []
 from Config_compare import *
-ConfigPredict.append(config_predict0())
-ConfigPredict.append(config_predict1())
-if nb_models=='3':
-    ConfigPredict.append(config_predict2())
-batchGenerating=ConfigPredict[0].batchGenerating
-path_configs = [c.model_configs for c in ConfigPredict]
-num0 = [c.predict_nums for c in ConfigPredict]
-tags = [c.tags for c in ConfigPredict]
-rmHFW = [c.rmHFW for c in ConfigPredict]
-'''
-maxNext = ConfigPredict.maxNext_JLX
-path_next = ConfigPredict.path_JLX_next
-path_simi = ConfigPredict.path_JLX_simi
-D_simi = json.load(open(path_simi,'r',encoding='utf-8'))
-D_next = json.load(open(path_next,'r',encoding='utf-8'))
-D_simi = {k:json.loads(D_simi[k]) for k in D_simi}
-D_next = {k:json.loads(D_next[k]) for k in D_next}
-'''
-D_simi,D_next,maxNext=[],[],[]
-model,tokenizer,config,device = [], [], [], []
-for ii in range(len(path_configs)):
-    m0,t0,c0,d0 = gpt_gen.getModel(path_config=path_configs[ii],gpu=ConfigPredict[ii].gpus)
-    c0['repetition_penalty'] = ConfigPredict[ii].repetition_penalty
-    c0['temperature'] = ConfigPredict[ii].temperature
-    c0['length'] = ConfigPredict[ii].length
-    model.append(m0)
-    tokenizer.append(t0)
-    config.append(c0)
-    device.append(d0)
 def predict(Data):
     quick = False
     R = []
@@ -56,4 +24,35 @@ def main(path_source,path_target):
 # start flask app
 if __name__ == '__main__':
     path_source,path_target = sys.argv[2:4]
+    nb_models = sys.argv[1]
+    ConfigPredict = []
+    ConfigPredict.append(config_predict0())
+    ConfigPredict.append(config_predict1())
+    if nb_models == '3':
+        ConfigPredict.append(config_predict2())
+    batchGenerating = ConfigPredict[0].batchGenerating
+    path_configs = [c.model_configs for c in ConfigPredict]
+    num0 = [c.predict_nums for c in ConfigPredict]
+    tags = [c.tags for c in ConfigPredict]
+    rmHFW = [c.rmHFW for c in ConfigPredict]
+    '''
+    maxNext = ConfigPredict.maxNext_JLX
+    path_next = ConfigPredict.path_JLX_next
+    path_simi = ConfigPredict.path_JLX_simi
+    D_simi = json.load(open(path_simi,'r',encoding='utf-8'))
+    D_next = json.load(open(path_next,'r',encoding='utf-8'))
+    D_simi = {k:json.loads(D_simi[k]) for k in D_simi}
+    D_next = {k:json.loads(D_next[k]) for k in D_next}
+    '''
+    D_simi, D_next, maxNext = [], [], []
+    model, tokenizer, config, device = [], [], [], []
+    for ii in range(len(path_configs)):
+        m0, t0, c0, d0 = gpt_gen.getModel(path_config=path_configs[ii], gpu=ConfigPredict[ii].gpus)
+        c0['repetition_penalty'] = ConfigPredict[ii].repetition_penalty
+        c0['temperature'] = ConfigPredict[ii].temperature
+        c0['length'] = ConfigPredict[ii].length
+        model.append(m0)
+        tokenizer.append(t0)
+        config.append(c0)
+        device.append(d0)
     main(path_source,path_target)
