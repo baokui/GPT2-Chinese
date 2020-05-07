93db28ff7561a737f2c46893fbaa98d154d83ace
baokui <sy880115>
2020-05-07 15:14:13 +0800

    gpt2-peom generating

---
+++ peom_generating/poemPro.py
@@ -0,0 +1,4 @@
+def pro(path_source,path_target):
+    with open(path_source,'r') as f:
+        s0 = f.read().strip().split('\n')
+    
--- test/app.py
+++ test/app.py
@@ -72,7 +72,8 @@ def test2():
         t0 = time.time()
         if data[-3:]=='#hd':
             result_poem = gpt_gen.generating_poem_head(app,data[:-3],model[0],config[0],tokenizer[0],device[0],num=10,gpu=ConfigPredict[0].gpus)
-            result_other = gpt_gen_thread.generating_thread(app, data, model[-2:], config[-2:], tokenizer[-2:], device[-2:], ConfigPredict[-2:],quick, num0,
+            result_poem = [r+ConfigPredict[0].tags for r in result_poem]
+            result_other = gpt_gen_thread.generating_thread(app, data[:-3], model[-2:], config[-2:], tokenizer[-2:], device[-2:], ConfigPredict[-2:],quick, num0,
                                                    removeHighFreqWordss=rmHFW, batchGenerating=batchGenerating,tags=tags,
                                                   D_simi=D_simi,D_next=D_next,maxNext=maxNext,maxChoice=10)
             result = result_poem+result_other
@@ -83,7 +84,8 @@ def test2():
                                                       D_simi=D_simi, D_next=D_next, maxNext=maxNext, maxChoice=10)
         elif data[-5:]=='#hdlv':
             result_poem = gpt_gen.generating_poem_head(app,data[:-5],model[1],config[1],tokenizer[1],device[1],num=10,gpu=ConfigPredict[0].gpus)
-            result_other = gpt_gen_thread.generating_thread(app, data, model[-2:], config[-2:], tokenizer[-2:],device[-2:], ConfigPredict[-2:], quick, num0,
+            result_poem = [r + ConfigPredict[1].tags for r in result_poem]
+            result_other = gpt_gen_thread.generating_thread(app, data[:-5], model[-2:], config[-2:], tokenizer[-2:],device[-2:], ConfigPredict[-2:], quick, num0,
                                                             removeHighFreqWordss=rmHFW, batchGenerating=batchGenerating,tags=tags,
                                                             D_simi=D_simi, D_next=D_next, maxNext=maxNext, maxChoice=10)
             result = result_poem + result_other
