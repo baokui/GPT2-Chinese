3cf66655c5f86e20835b75181da001fe424ed917
baokui <sy880115>
2020-05-07 14:07:06 +0800

    gpt2-peom generating

--- peom_generating/run_peomGenerator.sh
+++ peom_generating/run_peomGenerator.sh
@@ -6,39 +6,39 @@ path_config=../bin/config/config_poem.json
 
 
 idx0=0
-idx1=10000
+idx1=1000
 gpu=0
 nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=10000
-idx1=20000
+idx0=1000
+idx1=2000
 gpu=1
-#nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
+nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=20000
-idx1=30000
+idx0=2000
+idx1=3000
 gpu=2
-#nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
+nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=30000
-idx1=40000
+idx0=3000
+idx1=4000
 gpu=3
-#nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
+nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=40000
-idx1=50000
-gpu=4
-#nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
+idx0=4000
+idx1=5000
+gpu=0
+nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=50000
-idx1=60000
-gpu=5
-#nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
+idx0=5000
+idx1=6000
+gpu=1
+nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
-idx0=60000
-idx1=70000
-gpu=6
-#nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
+idx0=6000
+idx1=7000
+gpu=2
+nohup python -u peomGenerator.py $path_source $path_config $idx0 $idx1 $gpu $nsamples >> ./log/poemGen-$idx0-$idx1.log 2>&1 &
 
 idx0=70000
 idx1=80000
