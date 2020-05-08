22c5482ff26e22c63741a3a17352528302d646c1
baokui <sy880115>
2020-05-08 16:08:23 +0800

    poem-blackwords

--- demo_train/run_vpalog_dialogue.sh
+++ demo_train/run_vpalog_dialogue.sh
@@ -3,7 +3,7 @@ path_target0=data_vpa_dialogue/
 L0=(04 04 04 05 05 05)
 L1=(26 28 30 02 04 06)
 H=(00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23)
-n=0
+n=1
 for((i=0;i<6;i++))
 do
 for((j=0;j<24;j++))
@@ -13,12 +13,12 @@ pathtarget=$path_target0${L0[$i]}${L1[$i]}${H[$j]}.txt
 if [ -e $pathtarget ];
 then echo ok-$pathtarget;
 else
-    b=$(( $n % 2 ))
+    b=$(( $n % 5 ))
     if [ $b = 0 ]
     then
-    nohup python -u vpalog_dialogue.py $pathsource $pathtarget >> log/vpalog_d-${L0[$i]}${L1[$i]}${H[$j]}.log 2>&1 &
-    else
     nohup python -u vpalog_dialogue.py $pathsource $pathtarget >> log/vpalog_d-${L0[$i]}${L1[$i]}${H[$j]}.log 2>&1
+    else
+    nohup python -u vpalog_dialogue.py $pathsource $pathtarget >> log/vpalog_d-${L0[$i]}${L1[$i]}${H[$j]}.log 2>&1 &
     fi
     echo ok-$pathtarget;
     n=$[$n+1]
--- demo_train/vpalog_dialogue.py
+++ demo_train/vpalog_dialogue.py
@@ -3,26 +3,30 @@ import os
 import sys
 def tokenFilter(path_source,path_target,nb_min=8):
     #f = open(path_source,'r')
-    with open(path_source,'r') as f:
-        s = f.read().strip()
-    s = s.split()
+    #with open(path_source,'r') as f:
+        #s = f.read().strip()
+    #s = s.split()
+    f = open(path_source,'r')
     T = []
-    nb = 0
-    i0 = 0
-    i1 = i0+1
-    t0 = time.time()
-    while i1<len(s):
-        while s[i1]!='2':
-            if s[i1]!='0':
-                nb+=1
-            i1+=1
-        if nb>nb_min:
-            T.extend(s[i0:i1+1])
-        nb=0
-        i0 = i1+1
-        i1 = i1+1
-        if i0%6400000==0:
-            print(i0,len(T),i0/len(s))
+    N = 0
+    for line in f:
+        s = line.strip().split()
+        N+=len(s)
+        nb = 0
+        i0 = 0
+        i1 = i0+1
+        while i1<len(s):
+            while s[i1]!='2':
+                if s[i1]!='0':
+                    nb+=1
+                i1+=1
+            if nb>nb_min:
+                T.extend(s[i0:i1+1])
+            nb=0
+            i0 = i1+1
+            i1 = i1+1
+            if len(T)%6400000==0:
+                print(len(T)/64,N/64)
     T = ' '.join(T)
     with open(path_target,'w') as f:
         f.write(T)
