312c26b9c828d6d6c28ddd9036e832d0f05115d6
baokui <sy880115>
2020-05-08 15:41:05 +0800

    poem-blackwords

--- demo_train/run_vpalog_dialogue.sh
+++ demo_train/run_vpalog_dialogue.sh
@@ -3,18 +3,26 @@ path_target0=data_vpa_dialogue/
 L0=(04 04 04 05 05 05)
 L1=(26 28 30 02 04 06)
 H=(00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23)
+n=0
 for((i=0;i<6;i++))
 do
-for((j=0;j<23;j++))
+for((j=0;j<24;j++))
 do
 pathsource=$path_source0${L0[$i]}${L1[$i]}${H[$j]}.txt
 pathtarget=$path_target0${L0[$i]}${L1[$i]}${H[$j]}.txt
-nohup python -u vpalog_dialogue.py $pathsource $pathtarget >> log/vpalog_d-${L0[$i]}${L1[$i]}${H[$j]}.log 2>&1 &
-echo $pathsource
+if [ -e $pathtarget ];
+then echo ok-$pathtarget;
+else
+    b=$(( $n % 2 ))
+    if [ $b = 0 ]
+    then
+    nohup python -u vpalog_dialogue.py $pathsource $pathtarget >> log/vpalog_d-${L0[$i]}${L1[$i]}${H[$j]}.log 2>&1 &
+    else
+    nohup python -u vpalog_dialogue.py $pathsource $pathtarget >> log/vpalog_d-${L0[$i]}${L1[$i]}${H[$j]}.log 2>&1
+    fi
+    echo ok-$pathtarget;
+    n=$[$n+1]
+fi
+#echo $pathsource
 done
-j=23
-pathsource=$path_source0${L0[$i]}${L1[$i]}${H[$j]}.txt
-pathtarget=$path_target0${L0[$i]}${L1[$i]}${H[$j]}.txt
-nohup python -u vpalog_dialogue.py $pathsource $pathtarget >> log/vpalog_d-${L0[$i]}${L1[$i]}${H[$j]}.log 2>&1
-echo $pathsource
 done
