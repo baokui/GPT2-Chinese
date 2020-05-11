3a981d1144b73eb5a1b8cbe938612edbb7909c3b
baokui <sy880115>
2020-05-11 13:45:13 +0800

    prosepoem

--- test/gpt_gen_prosepoemp.py
+++ test/gpt_gen_prosepoemp.py
@@ -1,6 +1,27 @@
 from gpt_gen import *
 from Config import Config
 import sys
+def _is_chinese_char(char):
+    """Checks whether CP is the codepoint of a CJK character."""
+    # This defines a "chinese character" as anything in the CJK Unicode block:
+    #   https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_(Unicode_block)
+    #
+    # Note that the CJK Unicode block is NOT all Japanese and Korean characters,
+    # despite its name. The modern Korean Hangul alphabet is a different block,
+    # as is Japanese Hiragana and Katakana. Those alphabets are used to write
+    # space-separated words, so they are not treated specially and handled
+    # like the all of the other languages.
+    cp = ord(char)
+    if ((cp >= 0x4E00 and cp <= 0x9FFF) or  #
+            (cp >= 0x3400 and cp <= 0x4DBF) or  #
+            (cp >= 0x20000 and cp <= 0x2A6DF) or  #
+            (cp >= 0x2A700 and cp <= 0x2B73F) or  #
+            (cp >= 0x2B740 and cp <= 0x2B81F) or  #
+            (cp >= 0x2B820 and cp <= 0x2CEAF) or
+            (cp >= 0xF900 and cp <= 0xFAFF) or  #
+            (cp >= 0x2F800 and cp <= 0x2FA1F)):  #
+        return True
+    return False
 class config_predict_prosepoem(Config):
     # 定义构造方法
     def __init__(self, model_config='', gpus=''):  # __init__() 是类的初始化方法；它在类的实例化操作后 会自动调用，不需要手动调用；
@@ -80,7 +101,7 @@ def generating(prefix,model,config,tokenizer,config_predict,max_genlen = 1000,qu
         S = [prefix0+s[len(prefix):] for s in S]
     return S
 def poemFilter_prose(t,maxlen=12,minlen=2,minlines=6,maxlines=12,maxlen_smallSents=4,rate_smallSents=1/4):
-    punc_end = '，。！？?；'
+    punc_end = '，。！？?；！”'
     t = t.strip()
     s = ''
     for i in range(1,len(t)):
@@ -89,7 +110,8 @@ def poemFilter_prose(t,maxlen=12,minlen=2,minlines=6,maxlines=12,maxlen_smallSen
                 s += t[i-1]
         else:
             s += t[i-1]
-    s+=t[-1]
+    if _is_chinese_char(t[-1]):
+        s+=t[-1]
     s = s.split('\n')
     s = [tt.strip() for tt in s]
     S = []
