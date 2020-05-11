4bfa6236dd8ff8781fa6699048823fb695832503
baokui <sy880115>
2020-05-11 10:22:45 +0800

    prosepoem

--- test/run_gpt_gen_prosepoem.sh
+++ test/run_gpt_gen_prosepoem.sh
@@ -1,3 +1,3 @@
 path_source=data/test_prosepoem.txt
-path_target=rest/test_prosepoem_final.json
+path_target=result/test_prosepoem_final.json
 nohup python -u gpt_gen_prosepoemp.py $path_source $path_target >> log/pp.log 2>&1 &
