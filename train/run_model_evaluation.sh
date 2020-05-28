5248348bc9d94e7eeb1ba78ef605cf3f3070288d
baokui <sy880115>
2020-05-28 10:38:27 +0800

    test-evaluation

--- train/run_model_evaluation.sh
+++ train/run_model_evaluation.sh
@@ -1,3 +1,3 @@
 path_config=config/config_comments3.json
 path_data=data/test_taobao.txt
-nohup python -u model_evaluation.py $path_config $path_data >> log/eval.log 2>&1 &
+nohup python -u model_evalution.py $path_config $path_data >> log/eval.log 2>&1 &
