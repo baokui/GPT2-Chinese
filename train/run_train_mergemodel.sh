4c8d39df1a60fda6ae43234165574b6c1233ed7a
baokui <sy880115>
2020-06-04 15:34:46 +0800

    test-evaluation

---
+++ train/run_train_mergemodel.sh
@@ -0,0 +1,14 @@
+export CUDA_VISIBLE_DEVICES=3,1,2,0
+nohup python -u train_mergemodel.py \
+        --device=0,1,2,3 \
+        --model_config=model/model_merged/config.json \
+        --tokenizer_path=model/model_merged/vocab.txt \
+        --tokenized_data_path0=../data/userdata_mergedmodel/ \
+        --tokenized_data_path1=../data/godText_mergedmodel/ \
+        --epochs=100 \
+        --log_step=100 \
+        --min_length=8 \
+        --stride=64 \
+        --max_steps_perEpoch_perPiece=1000 \
+        --batch_size=64 \
+        --output_dir=model/model_merged/ >> log/train-mergemodel.log 2>&1 &
