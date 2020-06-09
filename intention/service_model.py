499a9100170a852546428ce499f13ba975026710
baokui <sy880115>
2020-06-09 17:02:59 +0800

    service_model

--- intention/service_model.py
+++ intention/service_model.py
@@ -3,21 +3,18 @@
 from __future__ import print_function
 
 import os
-import sys
 import time
 from datetime import timedelta
 from rnn_model import TRNNConfig, TextRNN, Tokenizer
-import numpy as np
 import tensorflow as tf
 from flask import Flask, request, Response
 from gevent.pywsgi import WSGIServer
 from gevent import monkey
-import random
 import json
 import logging
 monkey.patch_all()
 app = Flask(__name__)
-
+port = 1001
 max_len = 10
 #base_dir = sys.argv[1]
 #save_dir = sys.argv[2]
@@ -35,11 +32,6 @@ ckpt = tf.train.latest_checkpoint(save_dir)  # 找到存储变量值的位置
 # 创建session
 session = tf.Session()
 saver.restore(session, ckpt)
-def get_time_dif(start_time):
-    """获取已使用时间"""
-    end_time = time.time()
-    time_dif = end_time - start_time
-    return timedelta(seconds=int(round(time_dif)))
 def feed_data(x_batch, y_batch, keep_prob,model):
     feed_dict = {
         model.input_x: x_batch,
@@ -52,16 +44,28 @@ def test():
     r = request.json
     inputStr = r["input"]
     #inputStr = '想你'
-    x = [tokenizer.tokenization(inputStr[-max_len:], max_len=max_len)]
-    y = [[1, 0]]
-    feed_dict = feed_data(x, y, config.dropout_keep_prob, model)
-    feed_dict[model.keep_prob] = 1.0
-    modelpredict = tf.nn.softmax(model.logits)
-    predict_y = session.run(modelpredict, feed_dict=feed_dict)
-    predict_y = predict_y[:,0]
-    response = {'message': 'success', 'input': inputStr, 'result': predict_y}
+    try:
+        x = [tokenizer.tokenization(inputStr[-max_len:], max_len=max_len)]
+        y = [[1, 0]]
+        feed_dict = feed_data(x, y, config.dropout_keep_prob, model)
+        feed_dict[model.keep_prob] = 1.0
+        modelpredict = tf.nn.softmax(model.logits)
+        predict_y = session.run(modelpredict, feed_dict=feed_dict)
+        predict_y = predict_y[:,0][0]
+        response = {'message': 'success', 'input': inputStr, 'result': '%0.4f'%predict_y}
+        app.logger.error('INTENTION_SUCCESS\t' + json.dumps(response))
+    except Exception as e:
+        app.logger.error("error:",e)
+        response = {'message': 'error', 'input': inputStr, 'result': '-1','errMsg':str(e)}
+        app.logger.error('INTENTION_ERROR\t' + json.dumps(response))
     response_pickled = json.dumps(response)
     return Response(response=response_pickled, status=200, mimetype="application/json")
-if __name__ == '__main__':
-    test()
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 
+if __name__ == "__main__":
+    server  = WSGIServer(("0.0.0.0", port), app)
+    print("Server started")
+    server.serve_forever()
