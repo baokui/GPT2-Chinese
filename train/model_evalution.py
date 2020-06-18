065c0cd1fc4bb41e2aaf4d104644d5d8199d0c4c
baokui <sy880115>
2020-06-18 10:57:26 +0800

    model-evaluation-ppl

--- train/model_evalution.py
+++ train/model_evalution.py
@@ -50,7 +50,7 @@ def main(path_config,path_data,mask_tokens='MASK',gpu='3'):
         for s in data:
             id_msk = tokenizer.convert_tokens_to_ids('['+mask_token+']')
             context_tokens = [id_msk] + tokenizer.convert_tokens_to_ids(tokenizer.tokenize(s))
-            if len(data)==0:
+            if len(context_tokens)==1:
                 ppl = -1
             else:
                 ppl = getppl(model, context_tokens,tokenizer,device)
