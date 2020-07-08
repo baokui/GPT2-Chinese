1662fd3f989e58489e8baed5d38efd3374c75673
baokui <sy880115>
2020-07-08 20:34:48 +0800

    intention-finetune

--- train/run_datapro.sh
+++ train/run_datapro.sh
@@ -9,7 +9,7 @@ path_target=data1/tokens_godtext
 token_mask=[MASK_gou]
 maxline=200000
 mkdir path_target
-nohup python -u $path_vocab $path_source $path_target/god $token_mask $maxline >> log/god-$hour.log 2>&1 &
+nohup python -u datapro.py $path_vocab $path_source $path_target/god $token_mask $maxline >> log/god-$hour.log 2>&1 &
 
 t=1
 for((Hour=0;Hour<24;Hour++))
@@ -24,7 +24,7 @@ path_vocab=model/model_merged6/vocab.txt
 path_source=data1/vpalog/$hour
 path_target=data1/tokens_vpalog
 token_mask=[MASK_gou]
-maxline=200000
+maxline=1000000
 mkdir path_target
-nohup python -u $path_vocab $path_source $path_target/vpa-$hour $token_mask $maxline >> log/vpa-$hour.log 2>&1 &
+nohup python -u datapro.py $path_vocab $path_source $path_target/vpa-$hour $token_mask $maxline >> log/vpa-$hour.log 2>&1 &
 done
