74429e921dc9ee5b94bee244050e1f1d92dea1f9
baokui <sy880115>
2020-07-08 22:04:10 +0800

    intention-finetune

--- train/run_datapro.sh
+++ train/run_datapro.sh
@@ -1,6 +1,6 @@
 alias mhadoop='/search/odin/software/MarsJs/bin/hadoop'
 hadoopclients=ml_research,3evrqV2R
-mhadoop fs -Dhadoop.client.ugi=$hadoopclients -get VpaOutput_guobk/session_join_android_vpa_alldata/202007/07 data1/vpalog
+#mhadoop fs -Dhadoop.client.ugi=$hadoopclients -get VpaOutput_guobk/session_join_android_vpa_alldata/202007/07 data1/vpalog
 
 
 path_vocab=model/model_merged6/vocab.txt
@@ -8,7 +8,7 @@ path_source=data1/godtext
 path_target=data1/tokens_godtext
 token_mask=[MASK_gou]
 maxline=200000
-mkdir path_target
+mkdir $path_target
 nohup python -u datapro.py $path_vocab $path_source $path_target/god $token_mask $maxline >> log/god-$hour.log 2>&1 &
 
 t=1
@@ -25,6 +25,6 @@ path_source=data1/vpalog/$hour
 path_target=data1/tokens_vpalog
 token_mask=[MASK_gou]
 maxline=1000000
-mkdir path_target
+mkdir $path_target
 nohup python -u datapro.py $path_vocab $path_source $path_target/vpa-$hour $token_mask $maxline >> log/vpa-$hour.log 2>&1 &
 done
