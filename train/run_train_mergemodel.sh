267bfa5362d00ca2d8ed9e1bb9162f399dea6b4c
baokui <sy880115>
2020-07-10 13:04:30 +0800

    intention-finetune

--- train/run_train_mergemodel.sh
+++ train/run_train_mergemodel.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5
 nohup python -u train_mergemodel.py \
         --device=0,1,2,3,4,5 \
         --model_config=model/model_merged6_finetune/config.json \
