8a0d458d48552955a92e5d64fbc0eb5d0036b004
baokui <sy880115>
2020-07-24 18:26:50 +0800

    intention-finetune

--- train/run_train_mergemodel.sh
+++ train/run_train_mergemodel.sh
@@ -1,15 +1,15 @@
 export CUDA_VISIBLE_DEVICES=2,3,4,5,6,7
 nohup python -u train_mergemodel.py \
         --device=0,1,2,3,4,5 \
-        --model_config=model/model_merged6_finetune/config.json \
-        --tokenizer_path=model/model_merged6_finetune/vocab.txt \
+        --model_config=model/model_merged6_finetune_prosedata0604_0723/config.json \
+        --tokenizer_path=model/model_merged6_finetune_prosedata0604_0723/vocab.txt \
         --tokenized_data_path0=./data1/tokens_vpalog/ \
-        --tokenized_data_path1=./data/godText_mergedmodel/ \
+        --tokenized_data_path1=./data_prose_upscreen_tokens/ \
         --epochs=100 \
         --log_step=100 \
         --min_length=8 \
         --stride=64 \
         --max_steps_perEpoch_perPiece=1000 \
         --batch_size=96 \
-        --pretrained_model=model/model_merged6_finetune/pretrained/ \
-        --output_dir=model/model_merged6_finetune/ >> log/mergemodel6_finetune.log 2>&1 &
+        --pretrained_model=model/model_merged6_finetune/model_epoch10_step710000_loss-1.49/ \
+        --output_dir=model/model_merged6_finetune_prosedata0604_0723/ >> log/mergemodel6_finetune_prosedata0604_0723.log 2>&1 &
