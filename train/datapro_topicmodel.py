44ba3f644e4c580a97777548817526b0fdf5c246
baokui <sy880115>
2020-07-31 19:16:37 +0800

    train-topic model

---
+++ train/datapro_topicmodel.py
@@ -0,0 +1,82 @@
+import sys
+import os
+from collections import Counter
+def token_pad(line,full_tokenizer,subline,n_ctx,token_mask='[MASK]'):
+    punc = '.;!?。；！？'
+    tmp = [full_tokenizer.index(token_mask)]
+    tmp = tmp + subline
+    if len(tmp) > n_ctx - 1:
+        tmp = tmp[:n_ctx - 1]
+        idx_b = n_ctx-1
+    else:
+        tmp = tmp + (n_ctx - 1 - len(tmp)) * [full_tokenizer.index('[PAD]')]
+        idx_b = len(line)
+    line = line[idx_b:]
+    idx0 = 0
+    for p in punc:
+        if p in line:
+            idx0 = line.index(p)+1
+            break
+    line = line[idx0:]
+    tmp = tmp + [full_tokenizer.index('[CLS]')]
+    tmp = ' '.join([str(ss) for ss in tmp])
+    return tmp,line
+def build_files(path_source,path_target, full_tokenizer,token_mask,maxline, n_ctx=64,min_length=10,padding=True):
+    full_line = []
+    print('reading lines')
+    nb_lines = 0
+    files = os.listdir(path_source)
+    idx = 0
+    for file in files:
+        f = open(os.path.join(path_source,file), 'r', encoding='utf8')
+        for line in f:
+            nb_lines += 1
+            topics,line = line.strip().split('\t')
+            topics = topics.split('#')
+            if len(line)<min_length:
+                continue
+            subline = []
+            for topic in topics:
+                for a in topic:
+                    if a in full_tokenizer:
+                        subline.append(full_tokenizer.index(a))
+                    else:
+                        subline.append(full_tokenizer.index('[UNK]'))
+                subline.append(full_tokenizer.index('<T>'))
+            subline.append(full_tokenizer.index('<S>'))
+            for a in line:
+                if a in full_tokenizer:
+                    subline.append(full_tokenizer.index(a))
+                else:
+                    subline.append(full_tokenizer.index('[UNK]'))
+            if padding:
+                tmp,line = token_pad(line,full_tokenizer,subline,n_ctx,token_mask)
+                full_line.append(tmp)
+                while len(line)>n_ctx:
+                    tmp, line = token_pad(line,full_tokenizer,subline,n_ctx,token_mask)
+                    full_line.append(tmp)
+            else:
+                tmp = [full_tokenizer.index('[MASK]')]
+                tmp = tmp + subline
+                tmp = tmp + [full_tokenizer.index('[CLS]')]
+                full_line.extend(tmp)
+            if nb_lines%100000==0:
+                print('processing file %s with %d lines'%(file,nb_lines))
+            if len(full_line)>maxline:
+                with open(path_target+str(idx), 'w') as f:
+                    f.write('\n'.join(full_line))
+                full_line = []
+                idx += 1
+        f.close()
+    with open(path_target + str(idx), 'w') as f:
+        f.write('\n'.join(full_line))
+    print('finish')
+def main(path_vocab,path_source,path_target,token_mask,maxline):
+    #tokenizer_path = '../data/vocab/vocab_god_userdata.txt'
+    #tokenized_data_path = '../data/userdata_tokenized_new/'
+    full_tokenizer = open(path_vocab,'r').read().strip().split('\n')
+    build_files(path_source,path_target, full_tokenizer,token_mask,maxline)
+    #shutil.rmtree(data_path)
+if __name__=='__main__':
+    path_vocab,path_source,path_target,token_mask,maxline = sys.argv[1:]
+    main(path_vocab,path_source,path_target,token_mask,int(maxline))
---
+++ train/run_datapro_topicmodel.sh
@@ -0,0 +1,8 @@
+path_vocab=model/model_merged6/vocab.txt
+path_source=data1/godtext_topic/
+path_target=data1/tokens_godtext_topic
+mkdir $path_target
+token_mask=[MASK]
+maxline=200000
+mkdir $path_target
+nohup python -u datapro_topicmodel.py $path_vocab $path_source $path_target/god $token_mask $maxline >> log/god-0.log 2>&1 &
